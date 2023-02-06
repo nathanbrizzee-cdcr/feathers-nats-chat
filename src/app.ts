0b0f3bcdb256fe5ebadf4d8c19d96684f22af6e3
Nathan Brizzee <nathan.brizzee@cdcr.ca.gov>
2023-02-06 14:05:37 -0800

    Initial checkin

---
+++ src/app.ts
@@ -0,0 +1,67 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/application.html
+import { feathers } from '@feathersjs/feathers'
+import configuration from '@feathersjs/configuration'
+import { koa, rest, bodyParser, errorHandler, parseAuthentication, cors, serveStatic } from '@feathersjs/koa'
+import socketio from '@feathersjs/socketio'
+
+import type { Application } from './declarations'
+import { configurationValidator } from './configuration'
+import { logError } from './hooks/log-error'
+import { postgresql } from './postgresql'
+import { authentication } from './authentication'
+import { services } from './services/index'
+import { channels } from './channels'
+// const { Server } = require('feathers-mq');
+// @ts-expect-error
+import { Server }  from 'feathers-mq';
+
+const app: Application = koa(feathers())
+// set the name of app - required
+// @ts-expect-error
+app.set("name", "ServerName");
+
+// Load our app configuration (see config/ folder)
+app.configure(configuration(configurationValidator))
+
+// Set up Koa middleware
+app.use(cors())
+app.use(serveStatic(app.get('public')))
+app.use(errorHandler())
+app.use(parseAuthentication())
+app.use(bodyParser())
+
+// Configure services and transports
+app.configure(rest())
+app.configure(
+  socketio({
+    cors: {
+      origin: app.get('origins')
+    }
+  })
+)
+app.configure(postgresql)
+app.configure(authentication)
+app.configure(services)
+app.configure(channels)
+
+// Register hooks that run on all service methods
+app.hooks({
+  around: {
+    all: [logError]
+  },
+  before: {},
+  after: {},
+  error: {}
+})
+// Register application setup and teardown hooks here
+app.hooks({
+  setup: [],
+  teardown: []
+})
+// setup mq transport for server
+app.configure(Server({
+  url: 'nats.local', // hostname for NATS - optional (defaults to `localhost`)
+  port: 4222, // port(s) for NATS - optional (defaults to 4222)
+}));
+
+export { app }
